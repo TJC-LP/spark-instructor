569c4802608209fb6435ef9049fa69cff31947ab
Richie Caputo <rcaputo3@tjclp.com>
2024-10-16 15:05:20 -0400

    Update test_o1_factory.py
    - Mock o1 client

--- tests/factory/test_o1_factory.py
+++ tests/factory/test_o1_factory.py
@@ -1,7 +1,21 @@
+from unittest.mock import Mock, patch
+
 import pytest
 
 from spark_instructor.factory import O1Factory
 from spark_instructor.types.base import SparkChatCompletionMessages
+
+
+@pytest.fixture
+def mock_get_o1_aclient():
+    with patch("spark_instructor.factory.openai_factory.get_openai_aclient") as mock:
+        yield mock
+
+
+@pytest.fixture
+def o1_factory(mock_get_o1_aclient):
+    mock_get_o1_aclient.return_value = Mock()
+    return O1Factory.from_config()
 
 
 @pytest.fixture
@@ -15,8 +29,7 @@ def sample_messages(valid_image_url):
     )
 
 
-def test_o1_factory(sample_messages):
-    o1_factory = O1Factory.from_config()
+def test_o1_factory(sample_messages, o1_factory):
     assert o1_factory.format_messages(sample_messages) == [
         {"role": "user", "content": "Hello"},
         {"role": "assistant", "content": "Hi there!"},
